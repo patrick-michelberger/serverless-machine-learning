95496dcb55583320b23daa98ca62251fc54693eb
Patrick Michelberger <patrick.michelberger@tum.de>
2018-01-13 14:36:44 +0100

    init commit

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask import request
+from flask import json
+
+BUCKET_NAME = 'serverless-machine-learning'
+MODEL_FILE_NAME = 'model.pkl'
+MODEL_LOCAL_PATH = '/tmp/' + MODEL_FILE_NAME
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def index():
+    payload = json.loads(request.get_data().decode('utf-8'))
+    prediction = predict(payload['payload'])
+    data = {}
+    data['data'] = prediction[-1]
+    return json.dumps(data)
+
+
+def load_model():
+    print
+    'Loading model from S3'
+
+
+def predict(data):
+    print
+    'Making predictions'
+
+
+if __name__ == '__main__':
+    # listen on all IPs
+    app.run(host='0.0.0.0')
